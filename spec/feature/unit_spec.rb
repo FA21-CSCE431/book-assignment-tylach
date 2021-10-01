2e5b222f3d29c800a1889526c9c5252f40ea6e7d
tylach <tylach@tamu.edu>
2021-09-30 21:59:53 -0500

    Commiting the project

---
+++ spec/feature/unit_spec.rb
@@ -0,0 +1,24 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter')
+    described_class.new(author: 'JK Rowling')
+    described_class.new(price: 8.99)
+    described_class.new(publish_date: Time.new(1999, 8, 8))
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name, author, price, or publish date' do
+    subject.title = nil
+    subject.author = nil
+    subject.price = nil
+    subject.publish_date = nil
+
+    expect(subject).not_to be_valid
+  end
+end
